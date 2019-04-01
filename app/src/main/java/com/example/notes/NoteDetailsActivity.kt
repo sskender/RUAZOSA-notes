0a2d1852af6b590116ce161389629bc155cc1568
sskender <sskender@users.noreply.github.com>
2019-04-01 03:31:38 +0200

    NoteDetailsActivity now uses Intent result to create / edit notes

--- app/src/main/java/com/example/notes/NoteDetailsActivity.kt
+++ app/src/main/java/com/example/notes/NoteDetailsActivity.kt
@@ -1,34 +1,56 @@
 package com.example.notes
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.notes.model.Note
-import com.example.notes.model.NotesList
 import kotlinx.android.synthetic.main.activity_note_details.*
 import java.util.*
 
 class NoteDetailsActivity : AppCompatActivity() {
 
+    // change this depending on if a new note is created or the old one edited
+    private var thisIsANewNote = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_details)
+
+        // fill from intent extra (if note is being edited)
+        if (intent.extras != null) {
+            noteTitleEditText.setText(intent.extras?.get("title").toString())
+            noteDetailsEditText.setText(intent.extras?.get("details").toString())
+            thisIsANewNote = false
+        }
 
         // save button action
         saveNoteButton.setOnClickListener {
             val note = Note()
 
+            // grab values from text fields
             note.noteTitle = noteTitleEditText.text.toString()
             note.noteDetails = noteDetailsEditText.text.toString()
             note.noteTimestamp = Date()
 
-            NotesList.notesList.add(note)
+            // create new random uuid for new note
+            note.uuid = if (thisIsANewNote) UUID.randomUUID() else intent.extras?.get("uuid") as UUID
 
+            // fill result intent
+            val resultIntent = Intent()
+            resultIntent.putExtra("uuid", note.uuid)
+            resultIntent.putExtra("title", note.noteTitle)
+            resultIntent.putExtra("details", note.noteDetails)
+            resultIntent.putExtra("timestamp", note.noteTimestamp)
+
+            // send result code 1, note changed
+            setResult(1, resultIntent)
             finish()
         }
 
-
         // cancel button action
         cancelNoteButton.setOnClickListener {
+            // send result code zero, nothing new is done
+            setResult(0)
             finish()
         }
 
--- app/src/main/java/com/example/notes/model/Note.kt
+++ app/src/main/java/com/example/notes/model/Note.kt
@@ -4,6 +4,7 @@ import java.util.*
 
 class Note {
 
+    var uuid: UUID? = null
     var noteTitle: String? = null
     var noteDetails: String? = null
     var noteTimestamp: Date? = null
