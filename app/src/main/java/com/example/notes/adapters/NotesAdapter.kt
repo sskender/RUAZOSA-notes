aafa669a7d9cffc916dee6a6607f8d646d074aa6
sskender <sskender@users.noreply.github.com>
2019-04-14 21:39:15 +0200

    heavy adapter refactoring

--- app/src/main/java/com/example/notes/adapters/NotesAdapter.kt
+++ app/src/main/java/com/example/notes/adapters/NotesAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.notes.adapters
 
 import android.app.Activity
-import android.arch.persistence.room.Room
 import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -12,58 +11,74 @@ import android.widget.TextView
 import com.example.notes.NoteDetailsActivity
 import com.example.notes.R
 import com.example.notes.dao.AppDatabase
+import com.example.notes.dao.NotesList
 import com.example.notes.model.Note
-import com.example.notes.model.NotesList
+import java.text.SimpleDateFormat
+import java.util.*
 
-class NotesAdapter : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
+class NotesAdapter() : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
 
+    // only for view layout, nothing to do with database format
+    private val noteTimestampFormat = SimpleDateFormat("dd/MM/yy HH:mm", Locale.US)
+
+    // database connection
     private lateinit var db: AppDatabase
 
+
+    constructor(db: AppDatabase) : this() {
+        this.db = db
+    }
+
+
     /**
-     * Overrode RecyclerView methods,
-     * this will always work like this.
-     *
-     * Feel free to copy in any other projects.
+     * RecyclerView boilerplate
      */
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): NotesAdapter.NoteViewHolder {
         val context = p0.context
         val inflater = LayoutInflater.from(context)
         val notesListElement = inflater.inflate(R.layout.note_item_in_list, p0, false)
 
-        // db
-        db = Room.databaseBuilder(
-            context,
-            AppDatabase::class.java, "notes-db"
-        ).build()
-
         return NoteViewHolder(notesListElement)
     }
+
 
     override fun getItemCount(): Int {
         return NotesList.notesList.size
     }
 
+
     override fun onBindViewHolder(p0: NoteViewHolder, p1: Int) {
+        val currentNote: Note = NotesList.notesList[p1]
 
-        p0.bindNote(NotesList.notesList[p1])
+        // note properties (from inner class)
+        p0.noteTitleTextView?.text = currentNote.noteTitle
+        p0.noteTimestampTextView?.text = noteTimestampFormat.format(Date(currentNote.noteTimestamp)).toString()
 
-        // display note properties
-        p0.noteTitleTextView?.text = NotesList.notesList[p1].noteTitle
-        p0.noteTimestampTextView?.text =
-            NotesList.notesList[p1].noteTimestamp //p0.timestampFormat.format(NotesList.notesList[p1].noteTimestamp).toString()
+        // edit note (intent)
+        p0.itemView.setOnClickListener {
+            //Toast.makeText(p0.itemView.context, "Position is " + p1, Toast.LENGTH_SHORT).show()
+            val editNoteDetailsIntent = Intent(p0.itemView.context, NoteDetailsActivity::class.java)
 
-        // delete note option
+            editNoteDetailsIntent.putExtra("oldTitle", currentNote.noteTitle)
+            editNoteDetailsIntent.putExtra("oldDetails", currentNote.noteDetails)
+            editNoteDetailsIntent.putExtra("oldTimestamp", currentNote.noteTimestamp)
+
+            (p0.itemView.context as Activity).startActivityForResult(editNoteDetailsIntent, 0)
+        }
+
+        // delete note
         p0.noteDeleteButton?.setOnClickListener {
-            Thread(Runnable {
-                db.noteDao().deleteByUuid(NotesList.notesList[p1].uuid)
-            }).start()
+            //Toast.makeText(p0.itemView.context, "Delete is " + p1, Toast.LENGTH_SHORT).show()
             NotesList.notesList.removeAt(p1)
             this.notifyDataSetChanged()
+
+            Thread(Runnable {
+                db.noteDao()
+                    .deleteByProperties(currentNote.noteTitle, currentNote.noteDetails, currentNote.noteTimestamp)
+            }).start()
         }
+
     }
-    /*
-     * End of overrode methods.
-     */
 
 
     /**
@@ -76,44 +91,14 @@ class NotesAdapter : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
      */
     inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        // note timestamp format
-        //val timestampFormat = SimpleDateFormat("dd/MM/yy HH:mm")
-
-        // note_item_in_list properties
         var noteTitleTextView: TextView? = null
         var noteTimestampTextView: TextView? = null
         var noteDeleteButton: ImageButton? = null
 
-        // one note item in RecyclerView list
-        private var note: Note? = null
-
         init {
-            // find note_item_in_list properties
             noteTitleTextView = itemView.findViewById(R.id.noteTitleTextView)
             noteTimestampTextView = itemView.findViewById(R.id.noteTimestampTextView)
             noteDeleteButton = itemView.findViewById(R.id.deleteNoteImageButton)
-
-            // note is clickable
-            itemView.setOnClickListener {
-                // create intent to edit note
-                val editNoteDetailsIntent = Intent(itemView.context, NoteDetailsActivity::class.java)
-
-                // save note properties from current note to intent
-                editNoteDetailsIntent.putExtra("uuid", note?.uuid)
-                editNoteDetailsIntent.putExtra("title", note?.noteTitle)
-                editNoteDetailsIntent.putExtra("details", note?.noteDetails)
-
-                // start activity for note editing
-                (itemView.context as Activity).startActivityForResult(editNoteDetailsIntent, 0)
-            }
-        }
-
-        /**
-         * Binds note from database,
-         * so it is known which note is currently being worked on.
-         */
-        fun bindNote(note: Note) {
-            this.note = note
         }
 
     }
